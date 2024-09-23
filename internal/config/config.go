a0dcfdbeae1da70a423415b4bbd924f23616379c
ebower16 <dima.ponomarev.763@gmail.com>
2024-09-23 22:28:40 +0300

    Update config.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -14,7 +14,7 @@ func Load() (*Config, error) {
 	botToken := os.Getenv("BOT_TOKEN")
 	if botToken == "" {
 		log.Println("BOT_TOKEN not set; using default")
-		botToken = "your_default_bot_token_here" // Set your default bot token here if needed
+		botToken = "your_default_bot_token_here" 
 	}
 
 	return &Config{
